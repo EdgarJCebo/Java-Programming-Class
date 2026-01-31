5fabec1e934aaef33070ab4aff8cd22f95d39d8a
EdgarJCebo <ecebolle@student.scf.edu>
2026-01-31 00:01:45 -0500

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+//Public class for the rectangle
+public class Rectangle {
+    public static void main(String[] args) {
+
+        //Defines the Width and Height
+        double width = 4.5;
+        double height = 7.9;
+
+        //Calculates the Area and Perimeter
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        //Displays the results for the end
+        System.out.println("Area of the rectangle: " + area);
+        System.out.println("Perimeter of the rectangle: " + perimeter);
+    }
+}
